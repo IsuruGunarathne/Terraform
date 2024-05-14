2e99404ecc03f88261fd7ac8546112248be1f31e
Isuru <isuru623@gmail.com>
2024-05-14 11:17:03 +0530

    Fix typo

--- Azure/main.tf
+++ Azure/main.tf
@@ -5,15 +5,14 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0.2"
     }
-
-    backend "azurerm" {
-      resource_group_name   = ""
-      storage_account_name  = ""
-      container_name        = ""
-      key                   = ""
-    }
   }
 
+  backend "azurerm" {
+      resource_group_name  = ""
+      storage_account_name = ""
+      container_name       = ""
+      key                  = ""
+  }
   required_version = ">= 1.1.0"
 }
 
