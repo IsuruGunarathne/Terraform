cb619eabd494176621aced4c402a22306035de42
Isuru <isuru623@gmail.com>
2024-05-14 11:14:20 +0530

    Update backend

--- Azure/main.tf
+++ Azure/main.tf
@@ -5,6 +5,13 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0.2"
     }
+
+    backend "azurerm" {
+      resource_group_name   = ""
+      storage_account_name  = ""
+      container_name        = ""
+      key                   = ""
+    }
   }
 
   required_version = ">= 1.1.0"
